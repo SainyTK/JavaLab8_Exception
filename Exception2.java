ac1b94a9018f878175bc717bd2faec1b031aab6d
tanakorn0314 <tanakorn0412@gmail.com>
2018-04-09 12:38:24 +0700

    1

---
+++ Exception2.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+import java.io.FileNotFoundException;
+import java.io.File;
+
+public class Exception2
+{
+	public static void main(String[] args)
+	{
+		int total = 0;
+		int num = 0;
+
+		File myFile = null;
+		Scanner inputFile = null;
+
+		try
+		{
+			myFile = new File("inFile.txt");
+			inputFile = new Scanner(myFile);
+
+			while(inputFile.hasNext())
+			{
+					try{
+						num = inputFile.nextInt();
+						total += num;
+					}
+					catch(InputMismatchException ime)
+					{
+						System.out.println("Illegal value found");
+						inputFile.next();
+					}
+			}
+		}
+		catch(FileNotFoundException fnf)
+		{
+			System.out.println(fnf.getMessage());
+		}
+
+
+		System.out.println("The total value is " + total);
+	}
+}
